64872d8becee1a10ea52ed34c16169062c1c0d09
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-08-13 08:46:17 -0300

    01 - Criação do Projeto

---
+++ app/src/main/java/br/utfpr/projmeucandidato/ListarCandidatos.java
@@ -0,0 +1,14 @@
+package br.utfpr.projmeucandidato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarCandidatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_candidatos);
+    }
+}
